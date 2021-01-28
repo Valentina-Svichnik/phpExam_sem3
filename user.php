544371f8d7b978818febbbd7f5d04b1af18d6386
Valentina Svichnik <svichnikvalentina@gmail.com>
2021-01-28 15:57:48 +0300

    forms

--- user.php
+++ user.php
@@ -4,12 +4,19 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Форма</title>
-    <style>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Авторизация</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+      
+        <!-- Подключаем Bootstrap JS -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <!-- </head> -->
+    <!-- <body> -->
+    <!-- <style>
         * {
             margin: 0;
             padding: 0;
@@ -62,11 +69,11 @@
         }
 
 
-    </style>
+    </style> -->
 </head>
 <body>
     
-    <header>
+    <!-- <header>
         <img src="img/лого.jpg" alt="" id="logo">
         <ul class="header__menu">
             <li><a href="#">Главная</a></li>
@@ -74,7 +81,7 @@
             <li><a href="#">Контакты</a></li>
         </ul>
         
-    </header>
+    </header> -->
 
     <main style="display:flex;
                  flex-direction: column;
