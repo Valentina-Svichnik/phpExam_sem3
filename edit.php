881bd5790bfcd40f3a210c6b63bc57845fcdfe0f
Valentina Svichnik <svichnikvalentina@gmail.com>
2021-01-28 17:35:15 +0300

    work with bd

--- edit.php
+++ edit.php
@@ -3,65 +3,52 @@
 
 
 ?>
-<div class="edit_forms" style="display:flex; flex-direction:row">
-    <form action="edit.php/?r=edit" method="post" class="insert_form" style="display: flex;
-                                                                            flex-direction: column;
-                                                                            border: 1px solid black;
-                                                                            border-radius: 5px;
-                                                                            padding: 10px;
-                                                                            max-width: 300px;"
-    >
+<!DOCTYPE html>
+    <html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Авторизация</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+      
+        <!-- Подключаем Bootstrap JS -->
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    </head>
+    <body>
+
+<div class="container">
+    <form action="edit.php/?r=edit" method="post" class="w-50 p-5 mt-5 border border-dark rounded">
         <fieldset><legend>Редактирование сессии</legend>
-            <input type="text" placeholder="Название существующей сессии" name="sessionName">
-            <input type="number" placeholder="Введите id" name="id">
-            <input type="text" placeholder="Введите вопрос" name="question">
-            <input type="text" placeholder="Введите ответ" name="answer">
-            <input type="submit" name="button" value="Ввести значения">
+            <input type="text" placeholder="Название существующей сессии" name="sessionName" class="form-control w-50 m-1">
+            <input type="number" placeholder="Введите id" name="id" class="form-control w-50 m-1">
+            <input type="text" placeholder="Введите вопрос" name="question" class="form-control w-50  m-1">
+            <input type="text" placeholder="Введите ответ" name="answer" class="form-control w-50 m-1">
+            <input type="submit" name="button" value="Ввести значения" class="btn btn-success">
         </fieldset>
         
     </form>
 
-    <form action="edit.php/?r=delete" method="post" class="insert_form" style="display: flex;
-                                                                            flex-direction: column;
-                                                                            border: 1px solid black;
-                                                                            border-radius: 5px;
-                                                                            padding: 10px;
-                                                                            max-width: 300px;
-                                                                            margin-left: 10px;"
-    >
+    <form action="edit.php/?r=delete" method="post" class="w-50 p-5 mt-5 border border-dark rounded" >
         <fieldset><legend>Удалить сессию</legend>
-            <input type="text" placeholder="Наименование сессии" name="delName">
-            <input type="submit" value="Удалить сессию" name="button" style="margin-top: 65px;">
+            <input type="text" placeholder="Наименование сессии" name="delName" class="form-control w-50">
+            <input type="submit" value="Удалить сессию" name="button" style="margin-top: 65px;" class="btn btn-danger" >
         </fieldset>
     </form>
 
-    <form action="edit.php/?r=answer" method="post" class="insert_form" style="display: flex;
-                                                                            flex-direction: column;
-                                                                            border: 1px solid black;
-                                                                            border-radius: 5px;
-                                                                            padding: 10px;
-                                                                            max-width: 300px;
-                                                                            margin-left: 10px;"
-    >
+    <form action="edit.php/?r=answer" method="post" class="w-50 p-5 mt-5 border border-dark rounded" >
         <fieldset><legend>Удалить вариант ответа</legend>
-            <input type="text" placeholder="Наименование сессии" name="delName">
-            <input type="text" placeholder="Индентификатор вопроса" name="Qid">
-            <input type="submit" value="Удалить вариант ответа" name="button" style="margin-top: 65px;">
+            <input type="text" placeholder="Наименование сессии" name="delName" class="form-control w-50 m-1">
+            <input type="text" placeholder="Индентификатор вопроса" name="Qid" class="form-control w-50 m-1">
+            <input type="submit" value="Удалить вариант ответа" name="button" style="margin-top: 65px;" class="btn btn-danger">
         </fieldset>
     </form>
 
-    <form action="edit.php/?r=question" method="post" class="insert_form" style="display: flex;
-                                                                            flex-direction: column;
-                                                                            border: 1px solid black;
-                                                                            border-radius: 5px;
-                                                                            padding: 10px;
-                                                                            max-width: 300px;
-                                                                            margin-left: 10px;"
-    >
+    <form action="edit.php/?r=question" method="post" class="w-50 p-5 mt-5 border border-dark rounded">
         <fieldset><legend>Удалить вопрос</legend>
-            <input type="text" placeholder="Наименование сессии" name="delName">
-            <input type="text" placeholder="Индентификатор вопроса" name="Qid">
-            <input type="submit" value="Удалить вопрос" name="button" style="margin-top: 65px;">
+            <input type="text" placeholder="Наименование сессии" name="delName" class="form-control w-50 m-1">
+            <input type="text" placeholder="Индентификатор вопроса" name="Qid" class="form-control w-50 m-1">
+            <input type="submit" value="Удалить вопрос" name="button" style="margin-top: 65px;" class="btn btn-danger">
         </fieldset>
     </form>
 </div>
--- index.php
+++ index.php
@@ -15,104 +15,16 @@
       
         <!-- Подключаем Bootstrap JS -->
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
-    <!-- </head> -->
-    <!-- <body> -->
-    <!-- <style>
-                
-        main {
-            display: flex;
-            flex-direction: column;
-            /* background-color: #d1cebd; */
-            justify-content: center;
-            align-items: center;
-            max-width: 100%;
-            min-height: 100vh;
-            font-size: 1.8rem;
-        }
-
-        * {
-            margin: 0;
-            padding: 0;
-        }
-
-
-        li {
-            list-style: none;
-            list-style-type: none;
-        }
-
-        header {
-            display: flex;
-            justify-content: space-evenly;
-            width: 100%;
-            height: 9rem;
-            background-color: #424874;
-            font-size: 1.7rem;
-            color: #fff;
-            z-index: 1;
-        }
-        header a {
-            color: #fff;
-            text-decoration: none;
-        }
-
-        header .header__menu {
-            list-style: none;
-            display: flex;
-            align-items: center;
-            justify-content: space-around;
-            list-style-type: none;
-        }
-
-        .header__menu li {
-            margin-right: 3rem;
-        }
-
-        header h1 {
-            margin-top: 2rem;
-        }
-
-        #logo {
-            margin-left: 5rem;
-            justify-self: center;
-            /* position: absolute; */
-            padding: 1rem 3rem;
-            width: 7rem;
-            height: 7rem;
-            cursor: pointer;
-        }
-
-        .admin-form {
-            display: flex;
-            flex-direction: column;
-        }
-
-    </style> -->
-</head>
-<body>
-    
-
-    <!-- <header>
-        <img src="img/лого.jpg" alt="" id="logo">
-        <ul class="header__menu">
-            <li><a href="#">Главная</a></li>
-            <li><a href="#">Информация</a></li>
-            <li><a href="#">Контакты</a></li>
-        </ul>
-        
-    </header> -->
+   
     
     <main>
         <?php
-             //$link = mysqli_connect('localhost', 'root', '', 'expert');
             $link = mysqli_connect('std-mysql', 'std_938', 'qazwsxedc', 'std_938');
             if(mysqli_connect_errno()) {
                 echo 'Ошибка в подключении ('.mysqli_connect_errno().'): '.mysqli_connect_error().'<br>';
                 exit();
             } 
-            // else {
-            //     echo 'БД подключена!<br>';
-            // }
+            
         ?>
 
 
@@ -126,7 +38,7 @@
         </div>
         
 
-        <a href="user.php" style="color: black !important;">Форма</a>
+        <a href="user.php" style="color: black !important;">Пройти опрос без смс и регистрации!</a>
 
     </main>
         
